00ed714c43fa656ca3eca54dc82891d024f7c6fe
Stefan Radusi <stefan.radusi@aeratechnology.com>
2020-09-17 20:13:24 +0300

    format

--- src/components/Pages/MyMovies.js
+++ src/components/Pages/MyMovies.js
@@ -1,201 +1,181 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "../../App.css";
-import { generateUrl } from './SearchFunction/UrlSearch'
+import { generateUrl } from "./SearchFunction/UrlSearch";
 import { fetchMovies } from "./SearchFunction/FetchMovies.js";
- import Country from "./SearchFunction/CountryFilter"
-
-
+import Country from "./SearchFunction/CountryFilter";
 
 class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      Genre:null,
-Language:null,
-Country:null,
-data:null,
-input:"",
-runtime:0,
-Year:"",
-type:"",
-imdbRating:0,
-inputValue:"",
-isLoaded:false,
-loading:false,
-movies:[],
-error:"",
-filters:{'Title': '',
-'Genre': '',
-'Year': '',
-'Language': '',
-'Country': '',
-'Runtime': '',
-'imdbRating': '',},
-
-
-    }
+      Genre: null,
+      Language: null,
+      Country: null,
+      data: null,
+      input: "",
+      runtime: 0,
+      Year: "",
+      type: "",
+      imdbRating: 0,
+      inputValue: "",
+      isLoaded: false,
+      loading: false,
+      movies: [],
+      error: "",
+      filters: {
+        Title: "",
+        Genre: "",
+        Year: "",
+        Language: "",
+        Country: "",
+        Runtime: "",
+        imdbRating: "",
+      },
+    };
   }
 
   clearFilters = () => {
-    this.setState({
-      Genre:null,
-  Language:null,
-  Country:null,
-  input:"",
-  runtime:0,
-  Year:"",
-  data:null,
-  type:"",
-  imdbRating:0,
-  inputValue:"",
-  isLoaded:false,
-  loading:false,
-  movies:[],
-  error:"",
-  filters:{'Title': '',
-  'Genre': '',
-  'Year': '',
-  'Language': '',
-  'Country': '',
-  'Runtime': '',
-  'imdbRating': ''}
-    }, () => {
-
-      const url = generateUrl(this.state.filters);
-      fetchMovies(url)
-  .then((response) => {
-    if (!response.ok) {
-      throw response;
-    }
-    return response.json(); //we only get here if there is no error
-  })
-  .then((json) => {
-    this.setState({isLoaded:true, movies:json.results})
-
-  })
-
-
-    });
-  }
-
-
-
-  choicesUpdated = (filterTitle, value, ) => {
     this.setState(
-      prevState => {
-        let { filters } = prevState;
-        filters[filterTitle] = value;
-        console.log(filters)
-        return { filters };
+      {
+        Genre: null,
+        Language: null,
+        Country: null,
+        input: "",
+        runtime: 0,
+        Year: "",
+        data: null,
+        type: "",
+        imdbRating: 0,
+        inputValue: "",
+        isLoaded: false,
+        loading: false,
+        movies: [],
+        error: "",
+        filters: {
+          Title: "",
+          Genre: "",
+          Year: "",
+          Language: "",
+          Country: "",
+          Runtime: "",
+          imdbRating: "",
+        },
       },
-         ()=>{ let url = generateUrl(this.state.filters);
-          console.log(url);
-          fetchMovies(url)
-         
-          .then((json) => {
-            this.setState({isLoaded:true, movies:json.results})
-            console.log(json.results)
-            /*console.log(url)*/
-        
+      () => {
+        const url = generateUrl(this.state.filters);
+        fetchMovies(url)
+          .then((response) => {
+            if (!response.ok) {
+              throw response;
+            }
+            return response.json(); //we only get here if there is no error
           })
-    }
-         )
-
-  }
-
-
-  handleSearch  = (inputValue) => {
-    this.setState({
-      input: inputValue
-    })
-  }
-
-  handleOnSearchChange = (inputValue) => {
-
-    this.setState({ input: inputValue, loading: true });
-
-    this.choicesUpdated('Title', inputValue);
-
-  }
-
- 
-  handleCountryChange = Country => {
-
-
-    this.choicesUpdated('Country', Country.value);
-    this.setState({ Country });
-
+          .then((json) => {
+            this.setState({ isLoaded: true, movies: json.results });
+          });
+      }
+    );
   };
 
- 
+  choicesUpdated = (filterTitle, value) => {
+    this.setState(
+      (prevState) => {
+        let { filters } = prevState;
+        filters[filterTitle] = value;
+        console.log(filters);
+        return { filters };
+      },
+      () => {
+        let url = generateUrl(this.state.filters);
+        console.log(url);
+        fetchMovies(url).then((json) => {
+          this.setState({ isLoaded: true, movies: json.results });
+          console.log(json.results);
+          /*console.log(url)*/
+        });
+      }
+    );
+  };
+
+  handleSearch = (inputValue) => {
+    this.setState({
+      input: inputValue,
+    });
+  };
+
+  handleOnSearchChange = (inputValue) => {
+    this.setState({ input: inputValue, loading: true });
+
+    this.choicesUpdated("Title", inputValue);
+  };
+
+  handleCountryChange = (Country) => {
+    this.choicesUpdated("Country", Country.value);
+    this.setState({ Country });
+  };
 
   handleOnChange = (event) => {
     fetch(
-      `https://ancient-caverns-16784.herokuapp.com/movies?take=10&Title=`+event.target.value
+      `https://ancient-caverns-16784.herokuapp.com/movies?take=10&Title=` +
+        event.target.value
     )
-      .then(response => response.json())
-      .then(json => {
+      .then((response) => response.json())
+      .then((json) => {
         console.log(json.results);
-        this.setState({data:json.results})
+        this.setState({ data: json.results });
       });
-  
+
     this.setState({
-      input:event.target.value
-    })
-    console.log(event.target.value)
-  }
+      input: event.target.value,
+    });
+    console.log(event.target.value);
+  };
 
   componentDidMount() {
-
-    /*fetchMovies()
-    
-    .then((json) => {
-      this.setState({isLoaded:true, movies:json.results})
-  
-    })*/
+    fetchMovies().then((json) => {
+      this.setState({ isLoaded: true, movies: json.results });
+    });
   }
 
-  render() { 
-    
-    return(
+  render() {
+    return (
       <div>
-        <input placeholder="Search.." className="searchinput" type="text" value={this.state.input} SearchFilter={this.handleOnSearchChange} updateValue={this.handleSearch} onChange={this.handleOnChange}  />
-        <Country handleCountryChange={this.handleCountryChange} Country={this.state.Country} />
+        <input
+          placeholder="Search.."
+          className="searchinput"
+          type="text"
+          value={this.state.input}
+          SearchFilter={this.handleOnSearchChange}
+          updateValue={this.handleSearch}
+          onChange={this.handleOnChange}
+        />
+        <Country
+          handleCountryChange={this.handleCountryChange}
+          Country={this.state.Country}
+        />
 
-        {
-        this.state.data? 
-        <div> 
-          {this.state.data.map((item,index)=>
-             <img key={index} src={item.Poster}/>
-             )}
-        </div>
-        : <div>
-        {
-     this.state.movies? 
-     <div> 
-       {this.state.movies.map((item)=>
-          <img className="posters" src={item.Poster} />
-
-          )} 
-
-     </div>
-     :  ""
-       }
-        </div>
-          }
-        <div>
-        
-        </div>
-  
+        {this.state.data ? (
+          <div>
+            {this.state.data.map((item, index) => (
+              <img key={index} src={item.Poster} />
+            ))}
+          </div>
+        ) : (
+          <div>
+            {this.state.movies ? (
+              <div>
+                {this.state.movies.map((item) => (
+                  <img className="posters" src={item.Poster} />
+                ))}
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
+        )}
+        <div></div>
       </div>
-  
-  
-  
-  
-  
-    )
-  
-      
+    );
   }
 }
 
--- src/components/Pages/SearchFunction/CountryFilter.js
+++ src/components/Pages/SearchFunction/CountryFilter.js
@@ -1,46 +1,39 @@
-import { render } from '@testing-library/react';
-import React, { Component } from 'react';
-import "./CountryFilter.css"
-import Select from 'react-select';
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
+import "./CountryFilter.css";
+import Select from "react-select";
 
+const options = [
+  { value: "USA", label: "USA" },
+  { value: "UK", label: "UK" },
+  { value: "Romania", label: "Romania" },
+  { value: "Philippines", label: "Philippines" },
+  { value: "France", label: "France" },
+  { value: "Canada", label: "Canada" },
+  { value: "Japan", label: "Japan" },
+  { value: "Romania", label: "Romania" },
+  { value: "South Korea", label: "South Korea" },
+];
 
-const options=[
-{value:'USA', label:'USA'},
-{value:'UK', label:'UK'},
-{value:'Romania', label:'Romania'},
-{value:'Philippines', label:'Philippines'},
-{value:'France', label:'France'},
-{value:'Canada', label:'Canada'},
-{value:'Japan', label:'Japan'},
-{value:'Romania', label:'Romania'},
-{value:'South Korea', label:'South Korea'},
+class Country extends Component {
+  handleChange = (Country) => {
+    this.props.handleCountryChange(Country);
+  };
 
-
-
-]
-
-class Country extends Component{
-
-
-        handleChange = (Country) => {
-
-            this.props.handleCountryChange(Country);
-        }
-
-    render(){
-        const{Country}=this.props;
-        return(
-        <div className="country-container">
-        <Select  options={options} onChange={this.handleChange} value={Country} placeholder="Select Country" className="CountryClass"/>
-
-        </div>
-
-
-
-
-        )
-    }
-
+  render() {
+    const { Country } = this.props;
+    return (
+      <div className="country-container">
+        <Select
+          options={options}
+          onChange={this.handleChange}
+          value={Country}
+          placeholder="Select Country"
+          className="CountryClass"
+        />
+      </div>
+    );
+  }
 }
 
 export default Country;
