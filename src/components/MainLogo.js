136ba55c596c75bd069da6ce6f25216b2d317848
AlexEpu <65967586+AlexEpu@users.noreply.github.com>
2020-09-18 07:25:42 +0300

    deleted some files

--- src/components/MainLogo.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import food from "./../images/main-logo.png";
-
-import "./MainLogo.css";
-
-export function MainLogo() {
-  return <img src={food} alt="logo" className="main-logo" />;
-}
