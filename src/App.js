248d69719f2e08f69af45716a5a07f4f4fd459dd
AlexEpu <65967586+AlexEpu@users.noreply.github.com>
2020-09-18 07:23:39 +0300

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import "./App.css";
-import Home from "./components/Pages/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Movies from "./components/Pages/Movies";
+// import Movies from "./components/Pages/Movies";
 import MyMovies from "./components/Pages/MyMovies";
 import MyAccount from "./components/Pages/MyAccount";
 import JoinUs from "./components/Pages/JoinUs";
-import MovieDetails from "./components/Pages/MovieDetails";
 import Footer from "./components/footer";
 import Scroll from "./components/ScrollToTop";
-//import WelcomeBanner from "./components/WelcomeBanner";
+import GlobalUtils from "./GlobalUtils"
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/movies" component={Movies} />
+        {/* <Route path="/" exact component={Home} /> */}
+        {/* <Route path="/movies" component={Movies} /> */}
         <Route path="/mymovies" component={MyMovies} />
         <Route path="/myaccount" component={MyAccount} />
         <Route path="/join-us" component={JoinUs} />
-        <Route path="/movie-details" component={MovieDetails} />
       </Switch>
+      <GlobalUtils />
       <Footer />
       <Scroll showBelow={50} />
     </Router>
