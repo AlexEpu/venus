03277f73c37d044e784fe2a7b3b006b84ce11cb3
Adelina Cimpan <adelinacimpan@yahoo.com>
2020-09-19 10:34:00 +0300

    changes

--- src/components/Pages/Movies.js
+++ src/components/Pages/Movies.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "../../App.css";
 import WelcomeBanner from "../WelcomeBanner";
-import Search from "./Search"
 
 export default function Movies() {
   return (
     <>
-    <Search/>
+      <h1 className="mymovies"> MOVIES</h1>
     </>
   );
 }
--- src/components/Pages/MyMovies.js
+++ src/components/Pages/MyMovies.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "../../App.css";
 import WelcomeBanner from "../WelcomeBanner";
+import Search from "./Search"
+
 
 export default function MyMovies() {
   return (
     <>
-      <WelcomeBanner />
-      <h1 className="mymovies">MY MOVIES</h1>
-    </>
+      <Search/>
+      </>
   );
 }
