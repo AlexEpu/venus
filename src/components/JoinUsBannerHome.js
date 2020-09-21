fee442ed65ec39d39c4717ace4c2ca7ad9f9136e
Adelina Cimpan <adelinacimpan@yahoo.com>
2020-09-21 12:25:54 +0300

    done

--- src/components/JoinUsBannerHome.js
+++ src/components/JoinUsBannerHome.js
@@ -13,9 +13,10 @@ function JoinUsBannerHome() {
             <h1 className="join-us-main-container-text2">                        BE</h1>
             <h1 className="join-us-main-container-text3">        AMAZED</h1>
           </div>
-          <div className="button-container">
+          {!localStorage.getItem('accessToken')?  <div className="button-container">
             <Button />
-          </div>
+          </div> :""}
+         
         </div>
       </div>
     );
